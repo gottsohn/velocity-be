a698509998a42158ab69be4c67f367b252e6401e
Godson Ukpere <godson.ukpere@leapsome.com>
2025-12-31 21:59:27 +0100

    feat(RouteMap, package.json): add lint:fix command to package.json and adjust map zoom level and control settings in RouteMap component

--- www/src/components/RouteMap.tsx
+++ www/src/components/RouteMap.tsx
@@ -85,9 +85,9 @@ export function RouteMap({ streamData }: RouteMapProps) {
   return (
     <MapContainer
       center={defaultCenter}
-      zoom={50}
+      zoom={1000}
       style={{ height: '100%', width: '100%', borderRadius: '12px' }}
-      zoomControl={true}
+      zoomControl={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
